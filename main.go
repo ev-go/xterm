a81d09c1067eb6016ec31086587f1453588f917c
ev <dmitry_dtv@mail.ru>
2022-05-30 23:23:54 +0500

    220530
    trying adding terminal f

--- main.go
+++ main.go
@@ -4,8 +4,20 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 	"time"
 )
+
+func isP(s string) string {
+	mid := len(s) / 2
+	last := len(s) - 1
+	for i := 0; i < mid; i++ {
+		if s[i] != s[last-i] {
+			return "NO. It's not a Palimdrome."
+		}
+	}
+	return "YES! You've entered a Palindrome"
+}
 
 func main() {
 	requestUseUrl := "http://localhost"
@@ -16,6 +28,77 @@ func main() {
 	requestUseData := "data=21"
 
 	httpRequestString := requestUseUrl + requestUsePort + requestUseRout + "?" + requestUseLogin + "&" + requestUsePassword + "&" + requestUseData
+	// Menu
+	fmt.Println("\n********************************/ Menu /****************************************")
+	fmt.Println("\nThis is client for sending http requests to server")
+	fmt.Println("\nDefault URL: ", requestUseUrl, ";",
+		"\nDefault Port: ", requestUsePort, ";",
+		"\nDefault Rout: ", requestUseRout, ";",
+		"\nDefault Login: ", requestUseLogin, ";",
+		"\nDefault Password: ", requestUsePassword, ";",
+		"\nDefault Data: ", requestUseData, ";",
+		"\nDefault http request: ", httpRequestString, ";")
+
+	// fmt.Println("Логин")
+	// fmt.Scanf("%s\n", &Log)
+
+	// fmt.Println("Пароль")
+	// fmt.Scanf("%s\n", &Pass)
+	terminalInputSlice := []string{"Param", "value", "value1", "value2"}
+	fmt.Println(terminalInputSlice[0])
+	fmt.Println(terminalInputSlice[1])
+	fmt.Println(terminalInputSlice[2])
+	var readFromTerminal string
+	// var terminalParameter string
+	// var terminalValue string
+	helpChangeDefaults := "\nTo change defaults enter: <value to change> <new value> \nFor example: login user7 \nOr password 12345"
+	fmt.Println("\nDo you want to change defaults? (y/n)")
+	fmt.Scanf("%s\n", &readFromTerminal)
+	if readFromTerminal == "y" {
+		fmt.Println(helpChangeDefaults)
+		fmt.Scanf("%s\n", &readFromTerminal)
+		terminalInputSlice = strings.Fields(readFromTerminal)
+		fmt.Println(terminalInputSlice[0])
+		fmt.Println(terminalInputSlice[1])
+		fmt.Println(terminalInputSlice[2])
+	} else {
+		fmt.Println("No changes")
+	}
+	// if readFromTerminal == "y" {
+	// 	fmt.Println("\nWhat part of http request need to change?",
+	// 		"Write in terminal: 'url' or 'port' or 'rout' or 'login' or 'password' or 'data'")
+	// 	fmt.Scanf("%s\n", &readFromTerminal)
+	// 	if readFromTerminal == "login" {
+	// 		fmt.Println("\nEnter new login")
+	// 		fmt.Scanf("%s\n", &readFromTerminal)
+	// 		requestUseLogin = "login=" + readFromTerminal
+	// 		fmt.Println("\nLogin changed for:", requestUseLogin)
+	// 	}
+	// 	if readFromTerminal == "password" {
+	// 		fmt.Println("\nEnter new password")
+	// 		fmt.Scanf("%s\n", &readFromTerminal)
+	// 		requestUsePassword = "password=" + readFromTerminal
+	// 		fmt.Println("\nPassword changed for:", requestUsePassword)
+	// 	}
+	// 	if readFromTerminal == "data" {
+	// 		fmt.Println("\nEnter new data")
+	// 		fmt.Scanf("%s\n", &readFromTerminal)
+	// 		requestUseData = "data=" + readFromTerminal
+	// 		fmt.Println("\nData changed for:", requestUseData)
+	// 	}
+	// 	if readFromTerminal == "port" {
+	// 		fmt.Println("\nEnter new port")
+	// 		fmt.Scanf("%s\n", &readFromTerminal)
+	// 		requestUsePort = ":" + readFromTerminal
+	// 		fmt.Println("\nPort changed for:", requestUsePort)
+	// 	}
+	// } else {
+	// 	fmt.Println("No changes")
+	// }
+	httpRequestString = requestUseUrl + requestUsePort + requestUseRout + "?" + requestUseLogin + "&" + requestUsePassword + "&" + requestUseData
+
+	fmt.Println("\n******************************/ Menu End /**************************************")
+	//Menu end
 
 	bearer := "Bearer " + "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJBVFRFTlRJT04hIjoi0J_RgNC40LLQtdGCLCDQnNCw0LrRgSA6KSIsIkRhdGEgYW5zd2VyIGlzIjoiMjExIiwiVG9rZW4gcmVxdWVzdCBhdCI6IjIwMjItMDUtMjVUMjM6NDg6MzYuODAwNDU4MiswNTowMCIsImFkbWluIHBlcm1pc3Npb25zPyI6Im1heWJlIiwiZXhwIjoxNjUzNTY5MzE3LCJsb2dpbiI6InJvb3QxIn0.C6FekKeToH0j-G8GyiMegaoLtWODi9rOK-OM7ModS5Y"
 	cli := http.Client{Timeout: 5 * time.Second}
